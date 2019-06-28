261d260c9dc44255bb4b4c072348dcc5e5f38e1e
HL.Y <yanghaoliang94@gmail.com>
2019-06-28 15:42:09 +0800

    calendar

--- application/v4/handle/logic/RoomLogic.php
+++ application/v4/handle/logic/RoomLogic.php
@@ -143,7 +143,7 @@ class RoomLogic extends BaseService
             $id[] = (int)$v['id'];
             $list[$v['id']] = $v;
         }
-        if (!$id){
+        if (!$id) {
             return success(['list' => [], 'total_count' => 0]);
         }
 
@@ -234,185 +234,49 @@ class RoomLogic extends BaseService
         if (!isset($allParam['room_id'])) error(40000, '请填写room_id！');
         $room_id = encrypt($allParam['room_id'], 6, false);
 
-        if (isset($allParam['api_version'])) {
 
-            //因为需要分页，但是以月分页所以先统计月份
-            $total = $this->query->getCalendarTotal($channel, $sub_shop, $room_id, strtotime(date('Y-m-d', NOW)));
-            if (empty($total)) $total = 1;
-            else              $total = count($total);
-            $roomName = $this->query->getRoomInfoById($room_id);
-            if ($roomName->isEmpty()) error(40000, '无该房型信息!');
-            $roomStart = $roomName[0]['start'];
-            $roomEnd = $roomName[0]['end'];
+        $room = HotelRoomType::where('id', $room_id)->find();
 
-            $page = isset($allParam['page']) ? $allParam['page'] : 1;
-            $count = isset($allParam['count']) ? $allParam['count'] : 2;
-            if ($roomStart < NOW) {
-                $start = strtotime(date('Y-m-01 00:00:00', strtotime('+' . ($page * $count - $count) . ' month')));
+        $min = HotelBooking::field('date')
+            ->where('room', $room_id)
+            ->min('date', false);
+
+        $max = HotelBooking::field('date')
+            ->where('room', $room_id)
+            ->max('date');
+
+        $query = HotelBooking::field('date,sale_price,allot,used,status')
+            ->where('room', $room_id)
+            ->where('status', HotelBooking::STATUS_OPEN)
+            ->order('date');
+
+        if (isset($allParam['page'])) {
+            $page = startLimit($allParam);
+            $limit = $page['start'];
+            $length = $page['limit'] - 1;
+
+            if (isset($allParam['checkin']) && isset($allParam['checkout'])) {
+                $startTime = strtotime($allParam['checkin']);
+                $endTime = strtotime($allParam['checkout']);
             } else {
-                $start = strtotime(date('Y-m-01 00:00:00', strtotime('+' . ($page * $count - $count) . ' month', $roomStart)));
-            }
-
-            $end = strtotime('+' . ($count * $page) . ' month', $start);
-            if (isset($allParam['checkin'])) $start = strtotime($allParam['checkin']);
-            if (isset($allParam['checkout'])) $end = strtotime($allParam['checkout']);
-
-            $data = $this->query->getCalendar($channel, $sub_shop, $room_id, $start, $end);
-            $now = strtotime(date('Y-m-d', NOW));
-            foreach ($data as $n => $d) {
-                if ($d['date'] < $now) $data[$n]['status'] = 0;
-            }
-            $list = [];
-            $listCmpS = [];
-            $listCmpE = [];
-            if (!isset($allParam['checkin']) && !empty($data)) {
-                $completionS = ($data[0]['date'] - $start) / 86400;
-                $b = $completionS;
-                for ($a = 0; $a < $completionS; $a++) {
-                    $b--;
-                    $listCmpS[$b]['date'] = date('Y-m-d', $data[0]['date'] - ($a + 1) * 86400);
-                    $listCmpS[$b]['price'] = 0;
-                    $listCmpS[$b]['status'] = 0;
-                    $listCmpS[$b]['stock'] = 0;
-
-                }
-
-                $countLast = count($data);
-                $lastMonth = strtotime(date('Y-m-01', strtotime('+1 month', $data[$countLast - 1]['date'])));
-                if (date('m', $lastMonth) - date('m', $data[$countLast - 1]['date']) == 2) {
-                    $lastMonth = strtotime(date('Y-m-01', strtotime('+1 month', $data[$countLast - 1]['date']) - 1));
-                }
-                $lastMonth = strtotime('-1 day', $lastMonth);
-                $completionE = ($lastMonth - $data[$countLast - 1]['date']) / 86400;
-                for ($a = 1; $a <= $completionE; $a++) {
-                    $listCmpE[$a - 1]['date'] = date('Y-m-d', $data[$countLast - 1]['date'] + ($a) * 86400);
-                    $listCmpE[$a - 1]['price'] = 0;
-                    $listCmpE[$a - 1]['status'] = 0;
-                    $listCmpE[$a - 1]['stock'] = 0;
-
-                }
-
-            }
-            $roomStart = strtotime(date('Y-m-d', $roomStart));
-
-
-            $orderRoom = $this->query->orderRoom($room_id, $start, $end);
-            $orderData[$room_id] = [];
-            foreach ($orderRoom as $orderTime) {
-                if ($orderTime['checkin'] >= $start && $orderTime['checkout'] < $end) {
-                    $num = ($orderTime['checkout'] - $orderTime['checkin']) / (86400);
-                    for ($a = 0; $a < $num; $a++) {
-                        $time = $orderTime['checkin'] + $a * 86400;
-                        if (!isset($orderData[$orderTime['room_id']][$time])) $orderData[$orderTime['room_id']][$time] = 0;
-                        $orderData[$orderTime['room_id']][$time] = $orderData[$orderTime['room_id']][$time] + $orderTime['room_num'];
-                    }
-                }
-                if ($orderTime['checkin'] < $start && $orderTime['checkout'] > $start && $orderTime['checkout'] < $end) {
-                    $num = ($orderTime['checkout'] - $start) / (86400);
-                    for ($a = 0; $a < $num; $a++) {
-                        $time = $orderTime['checkin'] + $a * 86400;
-                        if (!isset($orderData[$orderTime['room_id']][$time])) $orderData[$orderTime['room_id']][$time] = 0;
-                        $orderData[$orderTime['room_id']][$time] = $orderData[$orderTime['room_id']][$time] + $orderTime['room_num'];
-                    }
-                }
-                if ($orderTime['checkin'] < $end && $orderTime['checkout'] > $end && $orderTime['checkin'] > $start) {
-                    $num = ($end - $orderTime['checkin']) / (86400);
-                    for ($a = 0; $a < $num; $a++) {
-                        $time = $orderTime['checkin'] + $a * 86400;
-                        if (!isset($orderData[$orderTime['room_id']][$time])) $orderData[$orderTime['room_id']][$time] = 0;
-                        $orderData[$orderTime['room_id']][$time] = $orderData[$orderTime['room_id']][$time] + $orderTime['room_num'];
-                    }
-                }
-                if ($orderTime['checkin'] <= $start && $orderTime['checkout'] > $end) {
-                    $num = ($end - $orderTime['checkin']) / (86400);
-                    for ($a = 0; $a < $num; $a++) {
-                        $time = $orderTime['checkin'] + $a * 86400;
-                        if (!isset($orderData[$orderTime['room_id']][$time])) $orderData[$orderTime['room_id']][$time] = 0;
-                        $orderData[$orderTime['room_id']][$time] = $orderData[$orderTime['room_id']][$time] + $orderTime['room_num'];
-                    }
-                }
-            }
-            $orderData = $orderData[$room_id];
-            empty($orderData) && $orderData = [];
-            $n = 0;
-            $start = 0;
-            foreach ($data as $k => $v) {
-                if ($start === 0) {
-                    $start = $v['date'];
-
-                } else {
-
-                    $dateNum = ($v['date'] - $start) / 86400;
-                    if ($dateNum > 1) {
-                        for ($i = 1; $i < $dateNum; $i++) {
-                            $list[$n]['status'] = 0;
-                            $list[$n]['price'] = 0;
-                            $list[$n]['date'] = date('Y-m-d', $start + $i * 86400);
-                            $list[$n]['stock'] = 0;
-                            $n++;
-                        }
-                    }
-                    $start = $v['date'];
-
-                }
-                if (!isset($orderData[$v['date']])) $orderData[$v['date']] = 0;
-                if ($v['status'] == '0') $v['status'] = 0;
-                if (($v['allot'] - $orderData[$v['date']]) <= '0') $v['status'] = 2;
-                if ($v['date'] < $roomStart || $v['date'] > $roomEnd) $v['status'] = 0;
-                $list[$n]['status'] = $v['status'];
-                $list[$n]['price'] = floatval($v['sale_price']);
-                $list[$n]['date'] = date('Y-m-d', $v['date']);
-                $list[$n]['stock'] = $v['allot'] - $orderData[$v['date']];
-                $n++;
-            }
-            if (!empty($listCmpS[0])) {
-                sort($listCmpS);
-                $list = array_merge($listCmpS, $list);
-            }
-            if (!empty($listCmpE[0])) {
-                sort($listCmpE);
-                $list = array_merge($list, $listCmpE);
-            }
-            $roomName = $roomName[0]['name'];
-            $booking = $list;
-        } else {//新方法
-
-            $room = HotelRoomType::where('id', $room_id)->find();
-
-            $min = HotelBooking::field('date')
-                ->where('room', $room_id)
-                ->min('date', false);
-
-            $max = HotelBooking::field('date')
-                ->where('room', $room_id)
-                ->max('date');
-
-            $query = HotelBooking::field('date,sale_price,allot,used,status')
-                ->where('room', $room_id)
-                ->where('status', HotelBooking::STATUS_OPEN)
-                ->order('date');
-
-            if (isset($allParam['page'])) {
-                $page = startLimit($allParam);
-                $limit = $page['start'];
-                $length = $page['limit'] - 1;
                 $startTime = strtotime(date('Y-m-01', strtotime("+ $limit  month", $min)));
                 $endTime = strtotime(date('Y-m-t', strtotime("+ $length month", $startTime)));
-                $query->where('date', '>=', $startTime);
-                $query->where('date', '<=', $endTime);
-                $total_count = ((date('Y', $max) - date('Y', $min)) * 12 + date('m', $max) - date('m', $min) + 1);
             }
-
-            $booking = $query->select()->hidden(['sale_price', 'allot', 'used'])->append(['price', 'stock'])->toArray();
-            $roomName = $room['name'];
+            $query->where('date', '>=', $startTime);
+            $query->where('date', '<=', $endTime);
+            $total_count = ((date('Y', $max) - date('Y', $min)) * 12 + date('m', $max) - date('m', $min) + 1);
         }
+
+        $booking = $query->select()->hidden(['sale_price', 'allot', 'used'])->append(['price', 'stock'])->toArray();
+        $roomName = $room['name'];
 
 
         success(['room_name' => $roomName, 'list' => $booking, 'total_count' => $total_count ?? 1]);
     }
 
 
-    public function tags($all_param)
+    public
+    function tags($all_param)
     {
         $request = request();
         $channel = encrypt($all_param['channel'], 3, false);//渠道id
