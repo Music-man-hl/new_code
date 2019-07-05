ab2a5d0b92e35e29e51b308844f19f017dc0a24e
Airlir <1271511453@qq.com>
2019-07-05 15:07:57 +0800

    fix bug

--- application/v4/handle/logic/ExtensionLogic.php
+++ application/v4/handle/logic/ExtensionLogic.php
@@ -428,9 +428,9 @@ class ExtensionLogic extends BaseService
         //判断是获取小程序二维码还是产品二维码
         if (isset($params['product_id']) && !empty($params['product_id'])) {
             if ($params['type'] == 1) {
-                $productId = encrypt($params['product_id'], 6);
+                $productId = encrypt($params['product_id'], 6, false);
             } else {
-                $productId = encrypt($params['product_id'], 1);
+                $productId = encrypt($params['product_id'], 1, false);
             }
             $shopId = $params['sub_shop_id'];
             $type = $params['type'];
