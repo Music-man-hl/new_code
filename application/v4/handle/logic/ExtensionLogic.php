9c9e9a2db0283faeaca7fdadb3eedddfbfcd5453
qhzhuis <qhzhui25@gmail.com>
2019-07-12 12:18:35 +0800

    fix qrcode error

--- application/v4/handle/logic/ExtensionLogic.php
+++ application/v4/handle/logic/ExtensionLogic.php
@@ -335,18 +335,15 @@ class ExtensionLogic extends BaseService
     public function product($channel, $userId, $params)
     {
         $page = $params['page'] ?? 1;
-        $product = DistributionProduct::alias('dp')
-            ->field('p.id,p.name,dp.shop_id as sub_shop_id,dp.type,p.cover as pic,p.price,dp.rate_type,dp.rate,dp.rate_all')
+        $query = DistributionProduct::alias('dp')
+            ->field('p.id,p.name,p.shop_id as sub_shop_id,dp.type,p.cover as pic,p.price,dp.rate_type,dp.rate,dp.rate_all')
             ->leftJoin(ProductUnion::getTable() . ' p', 'dp.id = p.id AND dp.type = p.type')
-            ->where(['dp.channel' => $channel, 'dp.status' => 1, 'p.status' => 1])
-            ->order("dp.create_time DESC")
+            ->where(['dp.channel' => $channel, 'dp.status' => 1, 'p.status' => 1]);
+
+        $product = $query->order("dp.create_time DESC")
             ->limit(($page - 1) * 5, 5)->select();
 
-        $count = DistributionProduct::alias('dp')
-            ->field('p.id,p.name,dp.shop_id as sub_shop_id,dp.type,p.cover as pic,p.price,dp.rate_type,dp.rate,dp.rate_all')
-            ->leftJoin(ProductUnion::getTable() . ' p', 'dp.id = p.id AND dp.type = p.type')
-            ->where(['dp.channel' => $channel, 'dp.status' => 1, 'p.status' => 1])
-            ->count();
+        $count = $query->count();
 
         //获取用户等级信息
         $userInfo = DistributionUser::field('userid,level')->where(['userid' => $userId])->find();
